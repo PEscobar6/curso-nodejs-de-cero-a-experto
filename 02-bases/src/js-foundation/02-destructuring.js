3b96b3752c6922030a2e3cb51e7534a5915be771
Pablo Escobar <PESCOB3@bancodebogota.com.co>
2023-10-24 22:00:42 -0500

    Configuración y migración del proyecto 02-bases a typescript

--- 02-bases/src/js-foundation/02-destructuring.js
+++
@@ -1,7 +0,0 @@
-console.log(process.env.PORT);
-
-const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
-
-const [ , , ,batman] = characters;
-
-console.log(batman);
--- 02-bases/src/plugins/get-id.plugin.js
+++
@@ -1,7 +0,0 @@
-const { v4: uuidv4 } = require('uuid');
-
-const getUuid = () => uuidv4();
-
-module.exports = {
-    getUuid,
-}
